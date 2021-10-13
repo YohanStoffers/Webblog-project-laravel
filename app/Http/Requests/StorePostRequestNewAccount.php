4ee55a02a6e0fdcb63f9340b5736fe6888933e3a
YohanStoffers <yohanstoffers18@gmail.com>
2021-10-13 15:49:40 +0200

    update day

---
+++ app/Http/Requests/StorePostRequestNewAccount.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePostRequestNewAccount extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'username'=> ['min:3','max:25','Unique:users,username','required'],
+            'email' => ['Email', 'Unique:users,email', 'required'],
+            'password' => ['min:5','max:255', 'alpha_dash','required'],
+            'author' => ['required', 'boolean'],
+            'premium' => ['required', 'boolean'],
+        ];
+    }
+}
